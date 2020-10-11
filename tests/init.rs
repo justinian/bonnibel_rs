1348a54849ebe1f4f82b42ac19f8beb6507801f2
Justin C. Miller <justin@justin.cm>
2020-10-10 18:00:14 -0700

    Fix test warning

--- tests/init.rs
+++ tests/init.rs
@@ -3,7 +3,7 @@ use predicates::prelude::*;
 use std::process::Command; // Run programs // Used for writing assertions
 
 #[test]
-fn file_doesnt_exist() -> Result<(), Box<std::error::Error>> {
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::main_binary()?;
     cmd.arg("-f").arg("test/file/doesnt/exist").arg("init");
     cmd.assert().failure().stderr(predicate::str::contains("No such file or directory"));
